5fb411afc04d98c087e6bd87683acc93cbbe2eca
Bartek Ciesielski <bartlomiej-ciesielski@wp.pl>
2020-03-23 23:49:22 +0100

    Updates

---
+++ precache-manifest.f6f2ce26c1db1dff6a51d49edfc46dc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc690fdc1c879483c08b8e391afd693",
+    "url": "/covid-charts-rect-app/index.html"
+  },
+  {
+    "revision": "626c2fe9d8803d7cc9b9",
+    "url": "/covid-charts-rect-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e7dae33c562d047cad76",
+    "url": "/covid-charts-rect-app/static/js/2.ae5d4166.chunk.js"
+  },
+  {
+    "revision": "1058cd71e6eeaea024950c49d01d42bb",
+    "url": "/covid-charts-rect-app/static/js/2.ae5d4166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626c2fe9d8803d7cc9b9",
+    "url": "/covid-charts-rect-app/static/js/main.ebf95237.chunk.js"
+  },
+  {
+    "revision": "b5ba971efc7e20844cbe",
+    "url": "/covid-charts-rect-app/static/js/runtime-main.a265f3a9.js"
+  }
+]);
